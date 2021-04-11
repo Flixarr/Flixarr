b87cb44b9359e1ccb07eea0522f0a0e0d3b671e3
Marc Hershey <marchershey@users.noreply.github.com>
2021-04-11 15:12:41 -0400

    created discover page

---
+++ app/Http/Controllers/Discover/DiscoverController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Discover;
+
+use App\Http\Controllers\Controller;
+
+class DiscoverController extends Controller
+{
+    public function view()
+    {
+        return view('web.discover.discover');
+    }
+}
