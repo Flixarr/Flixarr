571056ae5796d8dfd2212ef50f829d77b801b2da
Marc Hershey <marchershey@users.noreply.github.com>
2021-04-24 22:43:24 -0400

    purge

--- README.md
+++
@@ -1,9 +0,0 @@
-Plex API Links
-- none
-
-Themes
-- https://dmitryvolkov.me/demo/flixtv/index.html
-
-CSS
-- default "sections"
-    - bg-gray-800 px-6 py-4 rounded
--- resources/views/layouts/loading.blade.php
+++
@@ -1,7 +0,0 @@
-<x-minimal-layout>
-
-    <div class="absolute inset-0 flex items-center justify-center">
-        <x-loading size="20" />
-    </div>
-
-</x-minimal-layout>
