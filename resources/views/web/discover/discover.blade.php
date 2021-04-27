80c743d80aa178f58685dcc2978188cb2bcb4786
Marc Hershey <marchershey@users.noreply.github.com>
2021-04-27 03:30:52 -0400

    fix layout padding

--- resources/views/web/discover/discover.blade.php
+++ resources/views/web/discover/discover.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout hasTopNavigation hasBottomNavigation class="bg-red-500">
+<x-app-layout hasTopNavigation hasBottomNavigation>
     <div class="space-y-8">
         @livewire('discover.components.trending-slider')
         @livewire('components.media.container', ['containerType' => 'grid', 'title' => 'Popular Movies', 'tmdbEndpoint' => '/movie/popular', 'moreLink' =>'/asdf'])
